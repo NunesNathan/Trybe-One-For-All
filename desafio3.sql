99e800ad1ca86364763b36ece84016dae71d8820
NunesNathan <nunesxd7@hotmail.com>
2022-03-10 11:56:34 -0300

    :white_check_mark: (challenge) - Challenge 3

---
+++ desafio3.sql
@@ -0,0 +1,1 @@
+SELECT Users.username AS 'usuario', COUNT(*) AS 'qtde_musicas_ouvidas', ROUND(SUM(Musics.duration_seconds / 60), 2) AS 'total_minutos' FROM SpotifyClone.users AS Users INNER JOIN SpotifyClone.play_history AS History ON Users.user_id = History.user_id INNER JOIN SpotifyClone.musics AS Musics ON Musics.music_id = History.music_id GROUP BY Users.username;
