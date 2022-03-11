bbc31ab4a6038fff04ccb7aff7dfcf82ae5cdc40
NunesNathan <nunesxd7@hotmail.com>
2022-03-11 12:36:35 -0300

    :white_check_mark: (challenge) - Challenge 6 with decimal dataype

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,9 @@
-SELECT MIN(service_price) AS 'faturamento_minimo',
+-- decimal datatype https://stackoverflow.com/questions/1796334/mysql-preferred-column-type-for-product-prices
+
+SELECT ROUND(MIN(service_price), 2) AS 'faturamento_minimo',
  MAX(service_price) AS 'faturamento_maximo',
  ROUND(AVG(service_price), 2) AS 'faturamento_medio',
  ROUND(SUM(service_price), 2) AS 'faturamento_total'
-FROM SpotifyClone.services AS Services
-INNER JOIN SpotifyClone.users AS Users ON Users.service_id = Services.service_id;
+ FROM SpotifyClone.services AS Services
+ INNER JOIN SpotifyClone.users AS Users ON Users.service_id = Services.service_id;
+ 
